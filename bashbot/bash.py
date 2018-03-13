157e291ff4b2618f96b080960117307d7684cf09
adikso <adam@zambrzycki.net>
2018-03-13 21:58:21 +0100

    Add escape code alias

--- bashbot/bash.py
+++ bashbot/bash.py
@@ -79,6 +79,7 @@ class BashSession:
             '[F11]': u'\u001b[23~\u001b',
             '[F12]': u'\u001b[24~\u0008',
             '[<]': u'\u001b\u0005\u0015',  # ^E^U Clears input line
+            '<ESC>': u'\u001b',
             '\\a': '\a',
             '\\b': '\b',
             '\\f': '\f',
