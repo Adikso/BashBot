c3e8391955e43dc2d5bcd7fefaf077032c9c9f91
Adikso <adam@zambrzycki.net>
2019-11-06 02:44:27 +0100

    Add close default session

--- bashbot/command/close.py
+++ bashbot/command/close.py
@@ -1,16 +1,21 @@
 from discord.ext import commands
 
 from bashbot.command import has_permission
+from bashbot.exceptions import SessionDontExistException
 from bashbot.terminal.sessions import sessions
 
 
 class CloseCommand(commands.Cog):
     @commands.command(name='.close', aliases=['.c'])
     @has_permission('session.close')
-    async def close(self, ctx, name):
-        terminal = sessions().get_by_name(name)
+    async def close(self, ctx, name=None):
+        if name:
+            terminal = sessions().get_by_name(name)
+        else:
+            terminal = sessions().get_by_channel(ctx.message.channel)
+
         if not terminal:
-            return
+            raise SessionDontExistException()
 
         terminal.close()
         sessions().remove(terminal)
